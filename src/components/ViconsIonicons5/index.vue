571e7a8763ff13101dd74d75b62caecc924eb464
“hollow288” <571339274@qq.com>
2024-08-28 18:20:58 +0800

    vicons

--- src/components/ViconsIonicons5/index.vue
+++ src/components/ViconsIonicons5/index.vue
@@ -1,24 +1,44 @@
 <script setup lang="ts">
 import * as Ionicons5 from '@vicons/ionicons5';
+import {NotificationType, useMessage, useNotification} from "naive-ui";
+
+const message = useMessage()
+const notification = useNotification()
+
 
 onMounted(()=>{
-  for (const iconName in Ionicons5) {
-      const IconComponent = Ionicons5[iconName];
-      console.log(`Rendering icon: ${iconName}`);
-      // 在这里你可以使用 IconComponent 渲染图标
-      // 例如，假设你在 Vue 中使用：
-      // <IconComponent />
-    }
-
+  notify('info')
 })
+
+const copyImport = (iconName:String)=>{
+  let resultImport = `import ${iconName} from '@vicons/ionicons5/${iconName}'`;
+
+  navigator.clipboard.writeText(resultImport).then(() => {
+    message.success('已复制到粘贴板');
+  }).catch(err => {
+    message.error('复制失败: ' + err);
+  });
+}
+
+
+const notify = (type: NotificationType)=> {
+  notification[type]({
+    content: 'Ionicons5 图标',
+    meta: '右下角复制',
+    duration: 1500,
+    keepAliveOnHover: true
+  })
+}
+
 </script>
 
 <template>
   <main class="center-container">
     <div class="icon-grid">
       <div class="icon-item" v-for="(IconComponent, iconName) in Ionicons5" :key="iconName">
-        <n-card :title="iconName" style="width: 100%">
+        <n-card :title="iconName" style="width: 100%" size="small">
           <n-icon size="40" :component="IconComponent" />
+          <div class="copy-text" @click="copyImport(iconName)">Copy Import</div>
         </n-card>
       </div>
     </div>
@@ -36,11 +56,12 @@ html, body {
   justify-content: center;
   height: 100%;
   overflow-y: auto;
+  background-color: #f0f0f0;
 }
 
 .icon-grid {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   gap: 16px;
   padding: 16px;
 }
@@ -48,5 +69,21 @@ html, body {
 .icon-item {
   text-align: center;
 }
+
+.copy-text {
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
+  font-size: 12px;
+  color: #333;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  cursor: pointer;
+}
+
+.n-card:hover .copy-text {
+  opacity: 1;
+}
+
 </style>
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VirtualScroller from 'vue3-virtual-scroller';
+import 'vue3-virtual-scroller/dist/vue3-virtual-scroller.css';
 
 const app = createApp(App)
 app.use(router)
+app.use(VirtualScroller);
 app.mount('#app')
