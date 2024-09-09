238c39f8e0bf36e581379726d16b14748a9e6f3f
“hollow288” <571339274@qq.com>
2024-09-09 18:08:04 +0800

    img

--- src/components/Navigation/index.vue
+++ src/components/Navigation/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
   <main class="center-container">
-    <img src="../../../public/icon.svg" alt="Description of SVG" width="100" height="100" />
+    <img
+        src="../../../public/icon.svg"
+        alt="Description of SVG"
+        width="100"
+        height="100"
+        class="zoom-image"
+    />
   </main>
 </template>
 
@@ -15,5 +20,12 @@
   align-items: center; /* 垂直居中 */
   height: 100vh; /* 使 main 元素占满视口高度 */
 }
-</style>
 
+.zoom-image {
+  transition: transform 0.3s ease; /* 平滑过渡效果 */
+}
+
+.zoom-image:hover {
+  transform: scale(1.3); /* 鼠标悬停时放大 10% */
+}
+</style>
